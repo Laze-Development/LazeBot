048b977c6e2e3cb76d696a813de9b89b24bdcd6b
Bloectasy <261067marjan>
2020-10-21 13:54:42 +0200

    Relocating the Database init

--- src/Bot/events/guild/message.ts
+++ src/Bot/events/guild/message.ts
@@ -10,14 +10,14 @@ export = class MessageEvent extends Event {
     async run(message: Message) {
         if(!message.guild || !message.content.startsWith(prefix)) return;
 
+        if(!message.guild!.db) await message.guild!.init()
+        if(!message.member!.db) await message.member!.init()
+        if(!message.author!.db) await message.author!.init()
+
         const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
         const command = this.bot.commands.get(cmd) || this.bot.commands.find((c) => c.aliases.includes(cmd));
 
         if(command) command.run(message, args);
-
-        if(!message.guild!.db) await message.guild!.init()
-        if(!message.member!.db) await message.member!.init()
-        if(!message.author!.db) await message.author!.init()
 
     }
 }
